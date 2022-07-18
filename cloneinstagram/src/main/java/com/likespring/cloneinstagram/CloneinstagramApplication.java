000f80b6e2d796cae6baf8a8664c468ac3caf2a9
duyeong-ko <329_143@naver.com>
2022-07-18 16:35:48 +0900

    feat: initial front & images

---
+++ cloneinstagram/src/main/java/com/likespring/cloneinstagram/CloneinstagramApplication.java
@@ -0,0 +1,13 @@
+package com.likespring.cloneinstagram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneinstagramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneinstagramApplication.class, args);
+    }
+
+}
